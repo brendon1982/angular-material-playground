6210d9f25846ee34772e880e677e18d3f4c58c33
Brendon Page <brendon.page@chillisoft.co.za>
2022-03-15 23:02:28 +0400

    Added Angular Material. Added FAB button positioning example which swaps based on layout direction.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleListComponent } from './example-list/example-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"example-list", component: ExampleListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
